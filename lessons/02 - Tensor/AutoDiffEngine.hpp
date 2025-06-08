eebb926ac39db0322cd4c8e0c05f9983f8593c18
Harry Foster <harrywfoster10@gmail.com>
2025-06-08 13:47:05 +0100

    refactored autodiff-engine

--- lessons/02 - Tensor/AutoDiffEngine.hpp
+++ lessons/02 - Tensor/AutoDiffEngine.hpp
@@ -2,32 +2,32 @@
 
 template <typename T> class AutoDiffEngine {
 public:
-  void backward(Tensor<T> *output) {
-    std::vector<Tensor<T> *> topo_order;
-    std::unordered_set<Tensor<T> *> visited;
-    dfs(output, visited, topo_order);
-    if (output->getGrad().getData().empty()) {
-      if (output->getData().size() != 1) {
-        throw std::runtime_error(
-            "Output gradient initialization requires scalar output");
-      }
-      output->getGrad().setData({static_cast<T>(1)});
-    }
+  void backward(Tensor<T> &output) {
+    std::vector<std::shared_ptr<const Tensor<T>>> topo_order;
+    std::unordered_set<const Tensor<T> *> visited;
+    dfs(output.getSharedPtr(), visited, topo_order);
+
+    output.setGradOnes();
+    // it = input tensor
     for (auto it = topo_order.rbegin(); it != topo_order.rend(); ++it) {
+
       if ((*it)->requiresGrad() && (*it)->backwardsFunction) {
+        std::cout << "GRADIENT INPUT:" << std::endl;
+        (*it)->getGrad().print();
         (*it)->backwardsFunction(**it);
       }
     }
   }
 
 private:
-  void dfs(Tensor<T> *node, std::unordered_set<Tensor<T> *> &visited,
-           std::vector<Tensor<T> *> &order) {
-    if (visited.count(node))
+  void dfs(std::shared_ptr<const Tensor<T>> node,
+           std::unordered_set<const Tensor<T> *> &visited,
+           std::vector<std::shared_ptr<const Tensor<T>>> &order) {
+    if (visited.count(node.get()))
       return;
-    visited.insert(node);
+    visited.insert(node.get());
 
-    for (Tensor<T> *parent : node->getParents()) {
+    for (const auto &parent : node->parents) {
       dfs(parent, visited, order);
     }
 
